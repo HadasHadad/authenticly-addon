48c6b8b19cf2195aae882694ebec9385c25fdff8
bat678 <btylyyylw@gmail.com>
2026-05-07 18:41:46 +0300

    url.voted:true

--- popup.js
+++ popup.js
@@ -1,18 +1,39 @@
 function markImageAsVoted(imageUrl) {
     const cleanUrl = new URL(imageUrl).origin + new URL(imageUrl).pathname;
-  const data = {};
-  data[imageUrl] = { voted: true };
 
-  chrome.storage.local.set(data, () => {
-    console.log("Saved:", imageUrl);
-  });
-}
-function checkIfVoted(imageUrl) {
-  const cleanUrl = new URL(imageUrl).origin + new URL(imageUrl).pathname;
+    const data = {};
+    data[cleanUrl] = { voted: true };
 
-  chrome.storage.local.get([cleanUrl], (result) => {
-    console.log("Result:", result[cleanUrl]);
-  });
+    chrome.storage.local.set(data, () => {
+        console.log("Saved:", imageUrl);
+    });
 }
+
+function checkIfVoted(imageUrl, callback) {
+    const cleanUrl = new URL(imageUrl).origin + new URL(imageUrl).pathname;
+
+    chrome.storage.local.get([cleanUrl], (result) => {
+
+        console.log("Result:", result[cleanUrl]);
+
+        if (result[cleanUrl] && result[cleanUrl].voted === true) {
+            callback(true);
+        } else {
+            callback(false);
+        }
+    });
+}
+
+
+
 markImageAsVoted("https://example.com/image.jpg");
-checkIfVoted("https://example.com/image.jpg");
+
+checkIfVoted("https://example.com/image.jpg", (alreadyVoted) => {
+
+    if (alreadyVoted) {
+        console.log("Already voted -> show graph");
+    } else {
+        console.log("Not voted yet -> show voting buttons");
+    }
+
+});
