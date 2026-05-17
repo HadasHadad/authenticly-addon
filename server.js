708248e0e51919ca155d460014ed8872f54adac7
HadasHadad <123874368+HadasHadad@users.noreply.github.com>
2026-05-17 11:16:37 +0300

    תיקון באגים של אינטגרציה

--- server.js
+++ server.js
@@ -7,6 +7,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.static("public"));
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Private-Network', 'true');
+    next();
+});
 
 mongoose
   .connect("mongodb://127.0.0.1:27017/imagesDB")
@@ -100,3 +104,4 @@ const port = 3000;
 app.listen(port, () => {
   console.log(`server running on http://localhost:${port}`);
 });
+
